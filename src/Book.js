c54f9e69a2f46d10634f1529f909d3cdfc2e4e24
Carlos Ramirez <carlosjramirez11@gmail.com>
2023-04-04 19:09:28 -0700

    Added dynamic data to the book component

--- src/Book.js
+++ src/Book.js
@@ -1,15 +1,9 @@
-const Book = () => {
+const Book = ({ img, alt, title, author }) => {
   return (
     <article className='book'>
-      <img
-        src='https://m.media-amazon.com/images/I/513Y5o-DYtL.jpg'
-        alt='Atomic Habbits'
-      />
-      <h2>
-        Atomic Habits: An Easy & Proven Way to Build Good Habits & Break Bad
-        Ones
-      </h2>
-      <h4>James Clear</h4>
+      <img src={img} alt={alt} />
+      <h2>{title}</h2>
+      <h4>{author}</h4>
     </article>
   );
 };
--- src/BookList.js
+++ src/BookList.js
@@ -1,15 +1,35 @@
 import Book from './Book';
 
 const BookList = () => {
+  const firstBook = {
+    img: 'https://m.media-amazon.com/images/I/513Y5o-DYtL.jpg',
+    title:
+      'Atomic Habits: An Easy & Proven Way to Build Good Habits & Break Bad Ones',
+    author: 'James Clear',
+    alt: 'Atomic Habbits',
+  };
+
+  const secondBook = {
+    img: 'https://m.media-amazon.com/images/W/IMAGERENDERING_521856-T1/images/I/41F8JcJ4KuL.jpg',
+    title: 'Lessons in Chemistry: A Novel',
+    author: 'Bonnie Garmus',
+    alt: 'Lessons in Chemistry: A Novel',
+  };
+
   return (
     <section className='booklist'>
-      <Book />
-      <Book />
-      <Book />
-      <Book />
-      <Book />
-      <Book />
-      <Book />
+      <Book
+        img={firstBook.img}
+        title={firstBook.title}
+        author={firstBook.author}
+        alt={firstBook.alt}
+      />
+      <Book
+        img={secondBook.img}
+        title={secondBook.title}
+        author={secondBook.author}
+        alt={secondBook.alt}
+      />
     </section>
   );
 };
