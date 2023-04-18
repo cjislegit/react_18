11e3ccb3a0c5949e47cc8a647d3c1607418af013
Carlos Ramirez <carlosjramirez11@gmail.com>
2023-04-17 20:00:48 -0700

    Updated function in book

--- src/Book.js
+++ src/Book.js
@@ -1,8 +1,9 @@
-const Book = ({ book: { img, alt, title, author } }) => {
+const Book = ({ book: { img, alt, title, author, getBook, id } }) => {
   return (
-    <article className='book'>
+    <article className="book">
       <img src={img} alt={alt} />
       <h2>{title}</h2>
+      <input type="button" value="Click" onClick={() => getBook(id)} />
       <h4>{author}</h4>
     </article>
   );
--- src/BookList.js
+++ src/BookList.js
@@ -1,28 +1,33 @@
-import Book from './Book';
+import Book from "./Book";
 
 const BookList = () => {
   const books = [
     {
       id: 1,
-      img: 'https://m.media-amazon.com/images/I/513Y5o-DYtL.jpg',
+      img: "https://m.media-amazon.com/images/I/513Y5o-DYtL.jpg",
       title:
-        'Atomic Habits: An Easy & Proven Way to Build Good Habits & Break Bad Ones',
-      author: 'James Clear',
-      alt: 'Atomic Habbits',
+        "Atomic Habits: An Easy & Proven Way to Build Good Habits & Break Bad Ones",
+      author: "James Clear",
+      alt: "Atomic Habbits",
     },
     {
       id: 2,
-      img: 'https://m.media-amazon.com/images/W/IMAGERENDERING_521856-T1/images/I/41F8JcJ4KuL.jpg',
-      title: 'Lessons in Chemistry: A Novel',
-      author: 'Bonnie Garmus',
-      alt: 'Lessons in Chemistry: A Novel',
+      img: "https://m.media-amazon.com/images/W/IMAGERENDERING_521856-T1/images/I/41F8JcJ4KuL.jpg",
+      title: "Lessons in Chemistry: A Novel",
+      author: "Bonnie Garmus",
+      alt: "Lessons in Chemistry: A Novel",
     },
   ];
 
+  const getBook = (id) => {
+    const book = books.find((book) => book.id === id);
+    console.log(book);
+  };
+
   return (
-    <section className='booklist'>
+    <section className="booklist">
       {books.map((book) => {
-        return <Book book={book} key={book.id} />;
+        return <Book book={book} key={book.id} getBook={getBook} />;
       })}
     </section>
   );
