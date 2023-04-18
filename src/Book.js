6d57c1deebd8b04ed611f960de7173a1f0cbe5a4
Carlos Ramirez <carlosjramirez11@gmail.com>
2023-04-17 20:23:45 -0700

    Updated props in book list

--- src/Book.js
+++ src/Book.js
@@ -1,9 +1,10 @@
-const Book = ({ book: { img, alt, title, author, getBook, id } }) => {
+const Book = (props) => {
+  const { img, alt, title, author, getBook, id } = props;
   return (
-    <article className="book">
+    <article className='book'>
       <img src={img} alt={alt} />
       <h2>{title}</h2>
-      <input type="button" value="Click" onClick={() => getBook(id)} />
+      <input type='button' value='Click' onClick={() => getBook(id)} />
       <h4>{author}</h4>
     </article>
   );
--- src/BookList.js
+++ src/BookList.js
@@ -1,21 +1,21 @@
-import Book from "./Book";
+import Book from './Book';
 
 const BookList = () => {
   const books = [
     {
       id: 1,
-      img: "https://m.media-amazon.com/images/I/513Y5o-DYtL.jpg",
+      img: 'https://m.media-amazon.com/images/I/513Y5o-DYtL.jpg',
       title:
-        "Atomic Habits: An Easy & Proven Way to Build Good Habits & Break Bad Ones",
-      author: "James Clear",
-      alt: "Atomic Habbits",
+        'Atomic Habits: An Easy & Proven Way to Build Good Habits & Break Bad Ones',
+      author: 'James Clear',
+      alt: 'Atomic Habbits',
     },
     {
       id: 2,
-      img: "https://m.media-amazon.com/images/W/IMAGERENDERING_521856-T1/images/I/41F8JcJ4KuL.jpg",
-      title: "Lessons in Chemistry: A Novel",
-      author: "Bonnie Garmus",
-      alt: "Lessons in Chemistry: A Novel",
+      img: 'https://m.media-amazon.com/images/W/IMAGERENDERING_521856-T1/images/I/41F8JcJ4KuL.jpg',
+      title: 'Lessons in Chemistry: A Novel',
+      author: 'Bonnie Garmus',
+      alt: 'Lessons in Chemistry: A Novel',
     },
   ];
 
@@ -25,9 +25,9 @@ const BookList = () => {
   };
 
   return (
-    <section className="booklist">
+    <section className='booklist'>
       {books.map((book) => {
-        return <Book book={book} key={book.id} getBook={getBook} />;
+        return <Book {...book} key={book.id} getBook={getBook} />;
       })}
     </section>
   );
