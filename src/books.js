12680914e31a8f32f47289965f3e0f4cdf76a0ac
Carlos Ramirez <carlosjramirez11@gmail.com>
2023-05-09 20:22:39 -0700

    Moved the book array to its own file

---
+++ src/books.js
@@ -0,0 +1,17 @@
+export const books = [
+  {
+    id: 1,
+    img: 'https://m.media-amazon.com/images/I/513Y5o-DYtL.jpg',
+    title:
+      'Atomic Habits: An Easy & Proven Way to Build Good Habits & Break Bad Ones',
+    author: 'James Clear',
+    alt: 'Atomic Habbits',
+  },
+  {
+    id: 2,
+    img: 'https://m.media-amazon.com/images/W/IMAGERENDERING_521856-T1/images/I/41F8JcJ4KuL.jpg',
+    title: 'Lessons in Chemistry: A Novel',
+    author: 'Bonnie Garmus',
+    alt: 'Lessons in Chemistry: A Novel',
+  },
+];
